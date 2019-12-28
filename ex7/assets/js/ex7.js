c6017f14e98f8b5ff511cacd80d7129496837c35
Camille-t <57983280+Camille-t@users.noreply.github.com>
2019-12-28 14:06:54 +0100

    +

--- ex7/assets/js/ex7.js
+++ ex7/assets/js/ex7.js
@@ -7,17 +7,17 @@
 
 // On informe l'utilisateur au fur et à mesure qu'il avance dans le formulaire de la validité ou non de ce qu'il saisi(pour les champs obligatoires)
 
-document.getElementById("nom").addEventListener("blur", function (e) {
+document.getElementById("nom").addEventListener("blur", function (e) { // on ajoute un addEventListener sur le champ "nom". Lorsque l'utilisateur termine sa saisie et que le champ "nom" perd le focus (blur) on va tester la validité des données saisies
     var validiteNom = "";
-    var nom = e.target.value;
-    var regexNom = /^[a-zA-ZéèîïÉÈÎÏ][a-zéèêàçîï]+([-'\s][a-zA-ZéèîïÉÈÎÏ][a-zéèêàçîï]+)?$/;
-    if (regexNom.test(nom) === false && nom !== "") {
-        validiteNom = "Le nom saisi n'est pas valide.";
+    var nom = e.target.value; // on va lire la valeur de la chaine saisie par l'utilisateur 
+    var regexNom = /^[a-zA-ZéèîïÉÈÎÏ][a-zéèêàçîï]+([-'\s][a-zA-ZéèîïÉÈÎÏ][a-zéèêàçîï]+)?$/; // on définit une regExp pour le nom
+    if (regexNom.test(nom) === false && nom !== "") { // Si la fonction regex.test renvoie false pour le nom saisi par l'utilisateur et que le champ n'est pas laissé vide 
+        validiteNom = "Le nom saisi n'est pas valide."; //  on donne comme valeur à la variable validiteNom une chaine correspondant à un message d'erreur(d'aide à la saisie)
     }
-    else if (nom === "") {
-        validiteNom = "Vous devez saisir votre nom."
+    else if (nom === "") { // si l'utilisateur sort du champ nom sans avoir saisi une valeur(chaine)
+        validiteNom = "Vous devez saisir votre nom." // on donne comme valeur à la variable validiteNom un autre message d'erreur
     }
-    document.getElementById("aideNom").textContent = validiteNom;
+    document.getElementById("aideNom").textContent = validiteNom; // on affiche sous le champ nom dans le span id aideNom le message d'erreur correspondant
 });
 
 document.getElementById("prenom").addEventListener("blur", function (e) {
@@ -46,8 +46,9 @@ document.getElementById("email").addEventListener("blur", function (e) {
     document.getElementById("aideEMail").textContent = validiteEMail;
 });
 
-// Afin d'informer l'utilisateur de son erreur on va colorer le champ en rouge pâle
-function surligne(champ, erreur)
+// On informe l'utilisateur au moment de la validation du formulaire des champs restés invalides
+
+function surligne(champ, erreur) // La fonction surligne va colorer en rouge pâle le(s) champ(s) invalides
 {
     if (erreur) // si le second parametre de notre fonction vaut true, la fonction colore le champ
     {
@@ -55,14 +56,15 @@ function surligne(champ, erreur)
     }
     else
     {
-    champ.style.backgroundColor = ""; //Pour effacer le coloriage
+    champ.style.backgroundColor = ""; // Si les données saisies sont valides (c-a-d si erreur renvoi false), on laisse le(s) champ(s) transparent
     }
 };
-
-function verifNom(champ)
+// On créé trois fonctions pour verifier la validité des saisies utilisateur pour les champs nom, prénom et email.
+// Le reste des champs obligatoires est automatiquement vérifié par l'API formulaire d'HTML5 (à travers l'attribut required)
+function verifNom(champ) // On utilise une fonction verifNom qui va vérifier la validité des valeurs saisies par l'utilisateur (à travers la valeur du champ nom)
 {
     var regexNom = /^[a-zA-ZéèîïÉÈÎÏ][a-zéèêàçîï]+([-'\s][a-zA-ZéèîïÉÈÎÏ][a-zéèêàçîï]+)?$/;
-    if (!regexNom.test(champ.value)) // On utilise la methode test de notre regExp sur le texte saisi par l'utilisateur dans le champ nom. Si elle renvoie true cela signifie que les conditions de notre regExp ne sont pas respectées
+    if (!regexNom.test(champ.value)) // On utilise la methode test(str) de l'objet regExp sur le texte saisi par l'utilisateur dans le champ nom. Si elle renvoie true cela signifie que les conditions de notre regExp ne sont pas respectées
     {
         surligne(champ, true); // on appelle la fonction surligne avec true en deuxième paramètre donc notre champ est coloré
         return false; // on renvoie false pour notre fonction verifNom
@@ -118,20 +120,23 @@ function verifEMail(champ)
         return true;
     }
 };
-//Mega fonction
+// On créé une méga-fonction qui va appeler nos trois fonctions précédentes (verif...) et renvoyer true si tout est bon
 function verifForm(f)
 {
+    // On créé trois variables afin que le navigateur effectue les trois fonctions même si l'une d'elle renvoie false. 
+    // Si on ne procède pas ainsi le risque est que si l'une des fonctions (par ex verifNom) renvoie false, les deux autres ne seront 
+    // pas effectuées et donc même si les données saisies ne sont pas valides, les champs ne seront pas colorés en rouge.
     var nomOk = verifNom(f.nom);
     var prenomOk = verifPrenom(f.prenom);
     var eMailOk = verifEMail(f.email);
    
-    if (nomOk && prenomOk && eMailOk)
+    if (nomOk && prenomOk && eMailOk) // Si les trois variables renvoient true, la méga-fonction renvoie true et le formulaire est valide.
     {
         return true;
     } 
     else
     {
-        alert("Afin d'envoyer le formulaire vous devez remplir tous les champs en rouge")
+        alert("Afin d'envoyer le formulaire vous devez remplir tous les champs en rouge") // S'il y a des erreurs on affiche un message d'alerte à l'utilisateur lui indiquant que les champs colorés sont invalides
         return false;
     }
 };
