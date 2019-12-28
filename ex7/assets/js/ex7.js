2c90c034c33d0710b278f6131c1e95466ddb618d
Camille-t <57983280+Camille-t@users.noreply.github.com>
2019-12-28 03:28:43 +0100

    +

--- ex7/assets/js/ex7.js
+++ ex7/assets/js/ex7.js
@@ -6,6 +6,47 @@
  */
 
 // On informe l'utilisateur au fur et à mesure qu'il avance dans le formulaire de la validité ou non de ce qu'il saisi(pour les champs obligatoires)
+
+
+document.getElementById("nom").addEventListener("blur", function (e) {
+    var validiteNom = "";
+    var nom = e.target.value;
+    var regexNom = /^[a-zA-ZéèîïÉÈÎÏ][a-zéèêàçîï]+([-'\s][a-zA-ZéèîïÉÈÎÏ][a-zéèêàçîï]+)?$/;
+    if (regexNom.test(nom) === false && nom !== "") {
+        validiteNom = "Le nom saisi n'est pas valide.";
+    }
+    else if (nom === "") {
+        validiteNom = "Vous devez saisir votre nom."
+    }
+    document.getElementById("aideNom").textContent = validiteNom;
+});
+
+document.getElementById("prenom").addEventListener("blur", function (e) {
+    var validitePrenom = "";
+    var prenom = e.target.value;
+    var regexPrenom = /^[a-zA-ZéèîïÉÈÎÏ][a-zéèêàçîï]+([-'\s][a-zA-ZéèîïÉÈÎÏ][a-zéèêàçîï]+)?$/;
+    if (regexPrenom.test(prenom) === false && prenom !== "") {
+        validitePrenom = "Le prénom saisi n'est pas valide.";
+    }
+    else if (prenom === "") {
+        validitePrenom = "Vous devez saisir votre prénom."
+    }
+    document.getElementById("aidePrenom").textContent = validitePrenom;
+});
+
+document.getElementById("email").addEventListener("blur", function (e) {
+    var validiteEMail = "";
+    var email = e.target.value;
+    var regexEMail = /^\w+[\w-\.]*\@\w+((-\w+)|(\w*))\.[a-z]{2,3}$/g;
+    if (regexEMail.test(email) === false && email !== "") {
+        validiteEMail = "L'adresse mail saisie n'est pas valide.";
+    }
+    else if (email === "") {
+        validiteEMail = "Vous devez saisir votre adresse mail."
+    }
+    document.getElementById("aideEMail").textContent = validiteEMail;
+});
+
 // Afin d'informer l'utilisateur de son erreur on va colorer le champ en rouge pâle
 function surligne(champ, erreur)
 {
@@ -21,25 +62,19 @@ function surligne(champ, erreur)
 
 function verifNom(champ)
 {
-    var aideNom = document.getElementbyId("aideNom")
     var regexNom = /^[a-zA-ZéèîïÉÈÎÏ][a-zéèêàçîï]+([-'\s][a-zA-ZéèîïÉÈÎÏ][a-zéèêàçîï]+)?$/;
     if (!regexNom.test(champ.value)) // On utilise la methode test de notre regExp sur le texte saisi par l'utilisateur dans le champ nom. Si elle renvoie true cela signifie que les conditions de notre regExp ne sont pas respectées
     {
-        aideNom.innerText = "Le nom saisi n'est pas valide.";
-        aideNom.style.display ="inline";
         surligne(champ, true); // on appelle la fonction surligne avec true en deuxième paramètre donc notre champ est coloré
         return false; // on renvoie false pour notre fonction verifNom
     }
     else if (nom === "") // On s'assure que le champ n'est pas laissé vide par l'utilisateur
     {
-        aideNom.innerText = "Vous devez saisir votre nom."
-        aideNom.style.display = "inline";
         surligne(champ, true); // on appelle la fonction surligne avec true en deuxième paramètre donc notre champ est coloré
         return false; // on renvoie false pour notre fonction verifNom
     }
     else
     {
-        aideNom.style.display = "none";
         surligne(champ, false); // on appelle la fonction surligne avec false en deuxième paramètre donc notre champ n'est pas coloré
         return true; // on renvoie true pour notre fonction verifNom
     }
@@ -100,86 +135,4 @@ function verifForm(f)
         return false;
     }
 };
-
-/*
-document.getElementById("nom").addEventListener("blur", function (e) {
-    var validiteNom = "";
-    var nom = e.target.value;
-    var regexNom = /^[a-zA-ZéèîïÉÈÎÏ][a-zéèêàçîï]+([-'\s][a-zA-ZéèîïÉÈÎÏ][a-zéèêàçîï]+)?$/;
-    if (regexNom.test(nom) === false && nom !== "") {
-        validiteNom = "Le nom saisi n'est pas valide.";
-    }
-    else if (nom === "") {
-        validiteNom = "Vous devez saisir votre nom."
-    }
-    document.getElementById("aideNom").textContent = validiteNom;
-});
-
-document.getElementById("prenom").addEventListener("blur", function (e) {
-    var validitePrenom = "";
-    var prenom = e.target.value;
-    var regexPrenom = /^[a-zA-ZéèîïÉÈÎÏ][a-zéèêàçîï]+([-'\s][a-zA-ZéèîïÉÈÎÏ][a-zéèêàçîï]+)?$/;
-    if (regexPrenom.test(prenom) === false && prenom !== "") {
-        validitePrenom = "Le prénom saisi n'est pas valide.";
-    }
-    else if (prenom === "") {
-        validitePrenom = "Vous devez saisir votre prénom."
-    }
-    document.getElementById("aidePrenom").textContent = validitePrenom;
-});
-
-document.getElementById("email").addEventListener("blur", function (e) {
-    var validiteEMail = "";
-    var email = e.target.value;
-    var regexEMail = /^\w+[\w-\.]*\@\w+((-\w+)|(\w*))\.[a-z]{2,3}$/g;
-    if (regexEMail.test(email) === false && email !== "") {
-        validiteEMail = "L'adresse mail saisie n'est pas valide.";
-    }
-    else if (email === "") {
-        validiteEMail = "Vous devez saisir votre adresse mail."
-    }
-    document.getElementById("aideEMail").textContent = validiteEMail;
-});
-
-Lorsque l'utilisateur clique sur le bouton submit, la fonction ValidationEvent s'exécute :
-si les données saisies dans les champs obligatoires ne sont pas valides, 
-le formulaire n'est pas envoyé 
-document.getElementById("monFormulaire").addEventListener("submit", ValidationEvent);
-
-function ValidationEvent() {
-    // On enregistre les valeurs des champs dans des variables 
-
-    var nom = document.getElementById("nom").value;
-    var prenom = document.getElementById("prenom").value;
-    var email = document.getElementById("email").value;
-
-    // Nos RegExp
-    var regexNom = /^[a-zA-ZéèîïÉÈÎÏ][a-zéèêàçîï]+([-'\s][a-zA-ZéèîïÉÈÎÏ][a-zéèêàçîï]+)?$/;
-    var regexPrenom = /^[a-zA-ZéèîïÉÈÎÏ][a-zéèêàçîï]+([-'\s][a-zA-ZéèîïÉÈÎÏ][a-zéèêàçîï]+)?$/;
-    var regexEMail = /^\w+[\w-\.]*\@\w+((-\w+)|(\w*))\.[a-z]{2,3}$/g;
-
-    // Conditions
-    if (nom != "" && prenom != "" && email != "") { // On ne veut pas de champs vides
-        if (nom.match(regexNom)) { // condition pour la regExp du nom
-            if (prenom.match(regexPrenom)) { // condition pour la regExp du prenom
-                if (email.match(regexEMail)) { // condition pour la regExp de l'email
-                    if (document.getElementById("masculin").checked || document.getElementById("feminin").checked) { // condition pour que soit coché au moins un sexe
-                        alert("All type of validation has done on OnSubmit event.");
-                        return true;
-                    } else {
-                        alert("Vous devez choisir un genre.....!");
-                        return false;
-                    }
-                } else {
-                    alert("L'adresse mail n'est pas valide...!!!");
-                    return false;
-                }
-            } else {
-                alert("Vous devez remplir tous les champs obligatoires.....!");
-                return false;
-            }
-        }
-    }
-};
-console.log(ValidationEvent());
-*/
+console.log(verifForm(this));
